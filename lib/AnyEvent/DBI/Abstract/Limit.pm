025372eeb10ec8e3824a0f9ee704a81f808f9794
Nobuo Danjou <nobuo.danjou@gmail.com>
2010-09-07 19:33:32 +0900

    initial commit

---
+++ lib/AnyEvent/DBI/Abstract/Limit.pm
@@ -0,0 +1,33 @@
+package AnyEvent::DBI::Abstract::Limit;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::DBI::Abstract::Limit -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::DBI::Abstract::Limit;
+
+=head1 DESCRIPTION
+
+AnyEvent::DBI::Abstract::Limit is
+
+=head1 AUTHOR
+
+Nobuo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
